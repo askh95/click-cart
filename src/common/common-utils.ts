a06074f0a3d088c04872bd1f600812675fe11234
Askh <149861091+Xaxathon@users.noreply.github.com>
2024-09-13 17:39:41 +0300

    Initial commit

---
+++ src/common/common-utils.ts
@@ -0,0 +1,15 @@
+import type { Nil } from './common-types';
+
+export const APP_URL = 'https://next-shopper.vercel.app';
+export const APP_TITLE = 'Click&Cart';
+export const APP_DESCRIPTION = `${APP_TITLE} — это демонстрация простого полнофункционального e-commerce сайта, разработанного с использованием Next.js.`;
+export const APP_REPOSITORY_URL = 'https://github.com/askh95/click-cart';
+
+export const createMockArray = (length: number) => {
+  // eslint-disable-next-line unicorn/prefer-spread
+  return Array.from(Array.from({ length }).keys());
+};
+
+export const isNil = (value: unknown): value is Nil => {
+  return value === null || value === undefined;
+};
